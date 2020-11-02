888c60cd67d29d124f58e391e9466f755d0297b6
Jim Chan <jim@shoplineapp.com>
2020-11-03 01:44:24 +0800

    feat(page): handle the image upload and draw to canvas logic in home page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Home from "../views/Home.vue";
+import OldHome from "../views/OldHome.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes: Array<RouteConfig> = [
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/OldHome",
+    name: "OldHome",
+    component: OldHome
   },
   {
     path: "/about",
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="User can change anything here" />
+    Here should be the div for user to upload image
+    <input type="file" @change="onFileChange" name="img" accept="image/*" />
+    <div>
+      <canvas id="myCanvas" />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from "@/components/Home.vue";
 
 export default {
   name: "Home",
   components: {
     HelloWorld
+  },
+  data: () => {
+    return {
+      uploadedImage: null
+    }
+  },
+  methods: {
+    onFileChange(event){
+      console.log('event',event.target.result,'sds', URL.createObjectURL(event.target.files[0]));
+      // this.uploadedImage = URL.createObjectURL(event.target.files[0]);
+
+      if (event.target.files && event.target.files[0]) {
+        const FR = new FileReader();
+        FR.addEventListener("load", this.onFileLoad)
+        FR.readAsDataURL( event.target.files[0] );
+      }
+    },
+
+    onFileLoad (e) {
+        this.uploadedImage = e.target.result;
+        // this.uploadedImage = e.target.result;
+
+        //here are for the canvas worke
+        const myCanvas = document.getElementById("myCanvas");
+        const ctx = myCanvas.getContext("2d");
+        const image = new Image();
+        image.src = e.target.result;
+        image.onload = function() {
+          myCanvas.width = this.width;
+          myCanvas.height = this.height;
+          ctx.drawImage(image,0, 0 );
+        };
+    }
   }
 };
 </script>
